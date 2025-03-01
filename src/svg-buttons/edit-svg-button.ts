d1424af793d0273c12163570f6364762591df8ee
Conor Dowdall <conordowdallmail@gmail.com>
2025-03-01 20:49:58 +0000

    fix: update SVG file paths to use relative paths in svg-buttons

--- src/svg-buttons/edit-svg-button.ts
+++ src/svg-buttons/edit-svg-button.ts
@@ -1,7 +1,8 @@
 import SvgButton from "../svg-button.js";
 
 class EditSvgButton extends SvgButton {
-  static svgFilePath = "/icons/edit_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg";
+  static svgFilePath =
+    "../icons/edit_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg";
   static buttonAriaLabel = "Edit";
 }
 
